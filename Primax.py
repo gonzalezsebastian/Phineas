d3ce67e43c6e20149f0bff3c5b6c10952cdf76f4
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2022-02-27 18:39:02 -0500

    Update Primax.py
    Parsing the information from the Input file

--- Primax.py
+++ Primax.py
@@ -1,8 +1,52 @@
 import csv
 
-with open('input.csv') as file:
+def extractStation(stations):
+    #Tomar las estaciones
+    con = 0 #Cuantas estaciones se eliminan
+    totalCover = True #Controlar si no se cubre un espacio, en caso contrario mostrar el -1
+    
+    cont = 0
+    for station in stations:
+        stations[cont] = (station[0]-station[1],station[0]+station[1])
+        cont +=1
+
+    if(totalCover==True):
+        return con
+    else:
+        return -1
+
+coordinates = []
+
+#Extracting information from the input file
+with open('input.csv','r') as file:
     reader = csv.reader(file)
     for row in reader:
-        if row == ['0;0']:
-            break
-        print(row)     
+        coordinates.append(f"{row[0]}")
+
+
+cont = 0
+#Loading input into a list
+for coord in coordinates:
+    cord = coord.split(";")
+    if (cont==0):
+        cord[cont]=cord[0][3:5]
+    coordinates[cont] = cord
+    coordinates[cont][0] = int(coordinates[cont][0])
+    coordinates[cont][1] = int(coordinates[cont][1])
+    cont +=1
+
+finished = False
+
+#while(finished != True):
+#    extractStation(coordinates)
+#    aux = coordinates[0][1]
+#    print(aux)
+#    
+#    if(bool(coordinates)):
+#        finished = True
+
+#extractStation(coordinates)
+aux = coordinates[0][1]
+print(aux)
+del coordinates[0:aux+1]
+print(coordinates)
