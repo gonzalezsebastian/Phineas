74c425093dce9d1ba8d70f0eb61adaf31a3a721b
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2022-03-06 20:14:25 -0500

    Update Primax.py

--- Primax.py
+++ Primax.py
@@ -6,19 +6,25 @@ def extractStation(stations):
     totalCover = True #Controlar si no se cubre un espacio, en caso contrario mostrar el -1
     p = 1
     s=[]
-
+    #Se recorren las estaciones
     while p<=len(stations):
         stn = stations[p-1][1]
         cont = 0
         for e in range(p,p+stn):
+            #Se coloca el valor de coordenadas de las estaciones
             s.append([stations[e][0]-stations[e][1],stations[e][0]+stations[e][1]])
             print(s[0][0])
-            #rng1 = set(range(s[0][0],s[0][1]+1))
-            #rng2 = set(range(s[1][0],s[1][1]+1))
-            #rng3 = set(range(s[2][0],s[2][1]+1))
-            #rng1|=rng3
-            #rango = rng1.issuperset(rng2)
-            #print(rango)
+            #Se crea un primer rango desde el primer km de cubrimiento hasta el último de la primera estación
+            rng1 = set(range(s[0][0],s[0][1]+1))
+            #Se crea un primer rango desde el primer km de cubrimiento hasta el último de la segunda estación
+            rng2 = set(range(s[1][0],s[1][1]+1))
+            #Se crea un primer rango desde el primer km de cubrimiento hasta el último de la tercera estación
+            rng3 = set(range(s[2][0],s[2][1]+1))
+            #Se unen los rangos entre la primera y tercera estación
+            rng1|=rng3
+            #Con la función superset se verifica que el rango entre la primera y tercera estación contiene a la segunda, si es TRUE, se elimina la segunda estación
+            rango = rng1.issuperset(rng2)
+            print(rango)
         print(len(s))
         s = []
         p = p+1+stn
@@ -26,7 +32,7 @@ def extractStation(stations):
 
 coordinates = []
 
-#Extracting information from the input file
+#Extrayendo la información del archivo xlmx que convertí en csv para poder trabajarlo mejor
 with open('input.csv','r') as file:
     reader = csv.reader(file)
     for row in reader:
@@ -34,7 +40,7 @@ with open('input.csv','r') as file:
 
 
 cont = 0
-#Loading input into a list
+#Cargando la información extraída en una lista
 for coord in coordinates:
     cord = coord.split(";")
     if (cont==0):
@@ -48,8 +54,5 @@ for coord in coordinates:
 #    extractStation(coordinates)
 #    aux = coordinates[0][1]
 #    print(aux)
-
-aux = coordinates[0][1]
-auxL = coordinates[1:aux+1]
 
 extractStation(coordinates)
